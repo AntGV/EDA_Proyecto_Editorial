efc8189077093e7eefeac7d991bb230be413328e
AntGV <angarval@hotmail.es>
2025-04-30 18:46:10 +0200

    Update

--- PB_funciones.py
+++ PB_funciones.py
@@ -2,36 +2,112 @@ import numpy as np
 import pandas as pd
 import requests
 
+
+def nuevo_libro (diccionario, clave):
 # Función para añadir un nuevo libro a un diccionario determinado.
 # Se pasan como argumentos el diccionario y el código ISBN del libro.
-def nuevo_libro (diccionario, codigo_isbn):
-
-    # Se genera la consulta a través de una API
-    isbn = codigo_isbn
-    h = {'Authorization': "61066_f9ba2c2c66284291bfbe3153a53fdb85"} # ...............................Borrar API en entrega final!!!
-    response = requests.get(f"https://api2.isbndb.com/book/{isbn}", headers= h)
+    
+    # Se genera la consulta a través de una API en función del valor que se pase como argumento.    
+    if clave == int:
+        isbn = clave
+        h = {"Authorization": "61066_f9ba2c2c66284291bfbe3153a53fdb85"} # ...............................Borrar API en entrega final!!!
+        respuesta = requests.get(f"https://api2.isbndb.com/book/{isbn}", headers= h)
+   if clave == str:
+        title = clave
+        h = {"Authorization": "61066_f9ba2c2c66284291bfbe3153a53fdb85"} # ...............................Borrar API en entrega final!!!
+        respuesta = requests.get(f"https://api2.isbndb.com/books/{title}?page=1&pageSize=1&column=title&shouldMatchAll=1", headers= h)
 
     # Si la conexión se ha validado, se añaden los datos del libro consultado al diccionario. En caso contrario se lanza un mensaje de error.
-    if response.status_code == 200:
-            book_info = response.json().get('book', {})
+    if respuesta.status_code == 200:
+        book_info = respuesta.json().get("book", {})
+        if clave == str:
+            book_info = book_info[0]
+            
+        diccionario.update({
+            "ISBN": book_info.get("isbn13", ""),
+            "Título": book_info.get("title", ""),
+            "Autor": ", ".join(book_info.get("authors", [])),
+            "Editorial": book_info.get("publisher", ""),
+            "Páginas": book_info.get("pages", ""),
+            "Fecha de publicación": book_info.get("date_published", ""),
+            #"Subjects": ", ".join(book_info.get("subjects", [])),
+            #"Binding": book_info.get("binding", ""),
+            "Sinopsis": book_info.get("synopsis", ""),
+            "Lengua original": book_info.get("language", ""),
+            "Edición": book_info.get("edition", ""),
+            "Dimensiones": book_info.get("dimensions", ""),
+            #"MSRP": book_info.get("msrp", ""),
+            #"Image": book_info.get("image", ""),
+        })
+    else:
+            print(f"Error de ISBN {isbn}: {respuesta.status_code}")
+
+    return diccionario
+    
+
+def nuevo_libro_isbn (diccionario, codigo_isbn):
+# Función para añadir un nuevo libro a un diccionario determinado.
+# Se pasan como argumentos el diccionario y el código ISBN del libro.
+    
+    # Se genera la consulta a través de una API en función del valor que se pase como argumento.    
+    isbn = codigo_isbn
+    h = {"Authorization": "61066_f9ba2c2c66284291bfbe3153a53fdb85"} # ...............................Borrar API en entrega final!!!
+    respuesta = requests.get(f"https://api2.isbndb.com/book/{isbn}", headers= h)
+   
+    # Si la conexión se ha validado, se añaden los datos del libro consultado al diccionario. En caso contrario se lanza un mensaje de error.
+    if respuesta.status_code == 200:
+            book_info = respuesta.json().get("book", {})
             diccionario.update({
-                'ISBN': isbn,
-                'Title': book_info.get('title', ''),
-                'Author': ', '.join(book_info.get('authors', [])),
-                'Publisher': book_info.get('publisher', ''),
-                'Pages': book_info.get('pages', ''),
-                'Date Published': book_info.get('date_published', ''),
-                'Subjects': ', '.join(book_info.get('subjects', [])),
-                'Binding': book_info.get('binding', ''),
-                'Synopsis': book_info.get('synopsis', ''),
-                'Language': book_info.get('language', ''),
-                'Edition': book_info.get('edition', ''),
-                'Dimensions': book_info.get('dimensions', ''),
-                'MSRP': book_info.get('msrp', ''),
-                'Image': book_info.get('image', ''),
-                'Status': 'Success'
+                "ISBN": isbn,
+                "Título": book_info.get("title", ""),
+                "Autor": ", ".join(book_info.get("authors", [])),
+                "Editorial": book_info.get("publisher", ""),
+                "Páginas": book_info.get("pages", ""),
+                "Fecha de publicación": book_info.get("date_published", ""),
+                #"Subjects": ", ".join(book_info.get("subjects", [])),
+                #"Binding": book_info.get("binding", ""),
+                "Sinopsis": book_info.get("synopsis", ""),
+                "Lengua original": book_info.get("language", ""),
+                "Edición": book_info.get("edition", ""),
+                "Dimensiones": book_info.get("dimensions", ""),
+                #"MSRP": book_info.get("msrp", ""),
+                #"Image": book_info.get("image", ""),
             })
     else:
-            print(f"Error de ISBN {isbn}: {response.status_code}")
+            print(f"Error de ISBN {isbn}: {respuesta.status_code}")
+
+    return diccionario
+
+
+def nuevo_libro_titulo (diccionario, titulo):
+# Función para añadir un nuevo libro a un diccionario determinado.
+# Se pasan como argumentos el diccionario y el título del libro.
+    
+    # Se genera la consulta a través de una API en función del valor que se pase como argumento.   
+    title = titulo
+    h = {"Authorization": "61066_f9ba2c2c66284291bfbe3153a53fdb85"} # ...............................Borrar API en entrega final!!!
+    respuesta = requests.get(f"https://api2.isbndb.com/books/{title}?page=1&pageSize=1&column=title&shouldMatchAll=1", headers= h)
+   
+    # Si la conexión se ha validado, se añaden los datos del libro consultado al diccionario. En caso contrario se lanza un mensaje de error.
+    if respuesta.status_code == 200:
+            book_info = respuesta.json().get("books", {})
+            diccionario.update({
+                "ISBN": book_info[0].get("isbn13", ""),
+                "Título": book_info[0].get("title", ""),
+                "Autor": ", ".join(book_info[0].get("authors", [])),
+                "Editorial": book_info[0].get("publisher", ""),
+                "Páginas": book_info[0].get("pages", ""),
+                "Fecha de publicación": book_info[0].get("date_published", ""),
+                #"Subjects": ", ".join(book_info.get("subjects", [])),
+                #"Binding": book_info.get("binding", ""),
+                "Sinopsis": book_info[0].get("synopsis", ""),
+                "Lengua original": book_info[0].get("language", ""),
+                "Edición": book_info[0].get("edition", ""),
+                "Dimensiones": book_info[0].get("dimensions", ""),
+                #"MSRP": book_info.get("msrp", ""),
+                #"Image": book_info.get("image", ""),
+            })
+    else:
+            print(f"Error de ISBN {isbn}: {respuesta.status_code}")
 
     return diccionario
